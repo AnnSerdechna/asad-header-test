504a5f1ff7ba9116d7218551ae73977add93f001
Anna Serdechna <anserdechna@gmail.com>
2022-05-01 15:13:14 +0300

    Add GSAP3

--- src/index.js
+++ src/index.js
@@ -2,35 +2,41 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable func-names */
 
-const searchTopBtn = document.querySelector('.navTop__searchBtn');
-const searchForm = document.querySelector('.searchForm');
-const navLinks = document.querySelectorAll('.nav__link');
-const dropdowns = document.querySelectorAll('.dropdown');
+// const searchTopBtn = document.querySelector('.navTop__searchBtn');
+// const searchForm = document.querySelector('.searchForm');
+// const navLinks = document.querySelectorAll('.nav__link');
+// const dropdowns = document.querySelectorAll('.dropdown');
 
-searchTopBtn.addEventListener('click', () => {
-  searchForm.classList.toggle('visible');
-});
+// searchTopBtn.addEventListener('click', () => {
+//   searchForm.classList.toggle('visible');
+// });
 
-function liftDropdown() {
-  dropdowns.forEach((dropdown) => {
-    dropdown.style.display = 'none';
-  });
-}
+// function liftDropdown() {
+//   dropdowns.forEach((dropdown) => {
+//     dropdown.style.display = 'none';
+//   });
+// }
 
-navLinks.forEach((link) => {
-  link.addEventListener('click', function (e) {
-    const dropMenu = this.nextElementSibling;
+// navLinks.forEach((link) => {
+//   link.addEventListener('click', function (e) {
+//     const dropMenu = this.nextElementSibling;
 
-    if (dropMenu.style.display === 'block') {
-      dropMenu.style.display = 'none';
-    } else {
-      liftDropdown();
-      dropMenu.style.display = 'block';
-      e.stopPropagation();
-    }
-  });
-});
+//     gsap.from(dropMenu, {
+//       opacity: 0,
+//       duration: 1,
+//       y: -50,
+//     });
 
-window.addEventListener('click', () => {
-  liftDropdown();
-});
+//     if (dropMenu.style.display === 'block') {
+//       dropMenu.style.display = 'none';
+//     } else {
+//       liftDropdown();
+//       dropMenu.style.display = 'block';
+//       e.stopPropagation();
+//     }
+//   });
+// });
+
+// window.addEventListener('click', () => {
+//   liftDropdown();
+// });
