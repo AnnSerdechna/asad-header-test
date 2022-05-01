65dfc451a0a6ee5e0d3a44bba13f890d7b102c34
Anna Serdechna <anserdechna@gmail.com>
2022-05-01 16:04:55 +0300

    Fix cash bug

--- src/index.js
+++ src/index.js
@@ -2,41 +2,35 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable func-names */
 
-// const searchTopBtn = document.querySelector('.navTop__searchBtn');
-// const searchForm = document.querySelector('.searchForm');
-// const navLinks = document.querySelectorAll('.nav__link');
-// const dropdowns = document.querySelectorAll('.dropdown');
+const searchTopBtn = document.querySelector('.navTop__searchBtn');
+const searchForm = document.querySelector('.searchForm');
+const navLinks = document.querySelectorAll('.nav__link');
+const dropdowns = document.querySelectorAll('.dropdown');
 
-// searchTopBtn.addEventListener('click', () => {
-//   searchForm.classList.toggle('visible');
-// });
+searchTopBtn.addEventListener('click', () => {
+  searchForm.classList.toggle('visible');
+});
 
-// function liftDropdown() {
-//   dropdowns.forEach((dropdown) => {
-//     dropdown.style.display = 'none';
-//   });
-// }
+function liftDropdown() {
+  dropdowns.forEach((dropdown) => {
+    dropdown.style.display = 'none';
+  });
+}
 
-// navLinks.forEach((link) => {
-//   link.addEventListener('click', function (e) {
-//     const dropMenu = this.nextElementSibling;
+navLinks.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    const dropMenu = this.nextElementSibling;
 
-//     gsap.from(dropMenu, {
-//       opacity: 0,
-//       duration: 1,
-//       y: -50,
-//     });
+    if (dropMenu.style.display === 'block') {
+      dropMenu.style.display = 'none';
+    } else {
+      liftDropdown();
+      dropMenu.style.display = 'block';
+      e.stopPropagation();
+    }
+  });
+});
 
-//     if (dropMenu.style.display === 'block') {
-//       dropMenu.style.display = 'none';
-//     } else {
-//       liftDropdown();
-//       dropMenu.style.display = 'block';
-//       e.stopPropagation();
-//     }
-//   });
-// });
-
-// window.addEventListener('click', () => {
-//   liftDropdown();
-// });
+window.addEventListener('click', () => {
+  liftDropdown();
+});
