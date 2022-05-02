0d69d98db1eb947b46529f85eac4323c0a5d3e3e
Anna Serdechna <anserdechna@gmail.com>
2022-05-02 13:39:23 +0300

    Add logic for search form

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-/* eslint-disable no-console */
 /* eslint-disable no-param-reassign */
-/* eslint-disable func-names */
-
 const searchTopBtn = document.querySelector('.navTop__searchBtn');
+const searchFormBtn = document.querySelector('.searchForm__button');
+const searchFormInput = document.querySelector('.searchForm__input');
 const searchForm = document.querySelector('.searchForm');
 const navLinks = document.querySelectorAll('.nav__link');
 const dropdowns = document.querySelectorAll('.dropdown');
 
-searchTopBtn.addEventListener('click', () => {
-  searchForm.classList.toggle('visible');
+searchFormInput.addEventListener('change', (event) => {
+  searchFormInput.value = event.target.value;
+});
+
+searchFormBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  searchForm.style.display = 'none';
+});
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  searchForm.style.display = 'none';
+});
+
+searchTopBtn.addEventListener('click', (event) => {
+  if (searchForm.style.display === 'flex') {
+    searchForm.style.display = 'none';
+  } else {
+    searchForm.style.display = 'flex';
+
+    event.stopPropagation();
+  }
 });
 
 function liftDropdown() {
@@ -18,19 +39,27 @@ function liftDropdown() {
 }
 
 navLinks.forEach((link) => {
-  link.addEventListener('click', function (e) {
-    const dropMenu = this.nextElementSibling;
+  link.addEventListener('click', (event) => {
+    const dropMenu = event.target.nextElementSibling;
 
     if (dropMenu.style.display === 'block') {
       dropMenu.style.display = 'none';
     } else {
       liftDropdown();
+
       dropMenu.style.display = 'block';
-      e.stopPropagation();
+
+      event.stopPropagation();
     }
   });
 });
 
-window.addEventListener('click', () => {
+document.body.addEventListener('click', (event) => {
+  if (event.target !== searchFormInput) {
+    searchForm.style.display = 'none';
+
+    searchFormInput.value = '';
+  }
+
   liftDropdown();
 });
